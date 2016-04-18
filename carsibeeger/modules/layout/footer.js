deb46a60e27eca0cdb936a1de304f341a7b026d6
ibeeger <cxh80301983@126.com>
2016-04-18 10:21:47 +0800

    updata footer

--- carsibeeger/modules/layout/footer.js
+++ carsibeeger/modules/layout/footer.js
@@ -1,8 +1,8 @@
 /*
 * @Author: willclass
 * @Date:   2016-04-14 10:55:05
-* @Last Modified by:   willclass
-* @Last Modified time: 2016-04-14 10:56:44
+* @Last Modified by:   ibeeger
+* @Last Modified time: 2016-04-18 10:16:52
 */
 
 'use strict';
@@ -22,7 +22,7 @@ class Footer extends Component {
   constructor() {
     super();
     this.state = {
-      icons:["../../assets/home.png","../../assets/list.png","../../assets/shop.png","../../assets/help.png"],
+      icons:[require("../../assets/home.png"),require("../../assets/list.png"),require("../../assets/shop.png"),require("../../assets/help.png")],
       names:["主页","列表","购物车","我的"]
     }
     this.renderItem = this.renderItem;
@@ -36,14 +36,12 @@ class Footer extends Component {
 
   renderItem(item,i,_this){
     var text = _this.state.names[i]
-    var pic = require(item) || require("../../assets/list.png");
-        console.log(pic+"k"+item)
       return(
           <TouchableHighlight style={styles.touch} key={i} onPress={this.props.changePage} underlayColor="#c00">
             <View style={styles.cell}>
             <Image
              style={styles.icon}
-             source={pic}
+             source={item}
            />
            <Text style={styles.text}>{text}</Text>
             </View>
